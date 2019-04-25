068e8dadbd969b4fc72c4235b136a260c14a76e3
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-04-25 04:43:16 +0200

    Add fade in and preempt time functions

--- settings/DifficultySettings.cs
+++ settings/DifficultySettings.cs
@@ -49,5 +49,17 @@ namespace MapsetParser.settings
         /// <summary> Returns the radius of a circle or slider from the circle size. </summary>
         public float GetCircleRadius() =>
             32.0f * (1.0f - 0.7f * (circleSize - 5) / 5);
+
+        /// <summary> Returns the time from where the object begins fading in to where it is fully opaque.  </summary>
+        public double GetFadeInTime() =>
+            approachRate < 5
+                ? 1200 + 600 * (5 - approachRate) / 5
+                : 1200 - 750 * (approachRate - 5) / 5;
+
+        /// <summary> Returns the time from where the object is fully opaque to where it is on the timeline.  </summary>
+        public double GetPreemptTime() =>
+            approachRate < 5
+                ? 800 + 400 * (5 - approachRate) / 5
+                : 800 - 500 * (approachRate - 5) / 5;
     }
 }
