1b3d6ec77428920dfd2526eb752e77a591edf34f
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-04-30 08:05:08 +0200

    Add documentation to difficulty calculation

--- starrating/standard/StandardDifficultyCalculator.cs
+++ starrating/standard/StandardDifficultyCalculator.cs
@@ -9,15 +9,12 @@ namespace MapsetParser.starrating.standard
 {
     class StandardDifficultyCalculator
     {
-        // constants
         private const int       sectionLength          = 400;
-        private const double    difficultyMultiplier   = 0.0675; // 0.0675
-
-        // returns aim, speed and star rating respectively
-        // note that star rating isn't just the sum of aim and speed
+        private const double    difficultyMultiplier   = 0.0675;
+        
+        /// <summary> Returns a tuple of aim rating, speed rating, and star rating (calculated from the other two) respectively. </summary>
         public static Tuple<double, double, double> Calculate(Beatmap aBeatmap)
         {
-            // if there are no objects in the map it's star rating will be 0
             if (aBeatmap.hitObjects.Count == 0)
                 return new Tuple<double, double, double>(0, 0, 0);
 
@@ -27,29 +24,27 @@ namespace MapsetParser.starrating.standard
                 new Speed()
             };
 
-            // "the first object doesn't generate a strain, so we begin with an incremented section end"
+            // First object cannot generate strain, so we offset this to account for that.
             double currentSectionEnd = sectionLength;
-
-            // calc ignores the first object
+            
             foreach (HitObject hitObject in aBeatmap.hitObjects.Skip(1))
             {
-                // perform all this on the previous object, which is why it's before Process()
+                // Performed on the previous object, hence before Process.
                 while (hitObject.time > currentSectionEnd)
                 {
                     foreach (Skill skill in skills)
                     {
-                        skill.SaveCurrentPeak();                          // adds new peak
-                        skill.StartNewSectionFrom(currentSectionEnd);   // decays from prev object's start to the start of the new section
+                        skill.SaveCurrentPeak();
+                        skill.StartNewSectionFrom(currentSectionEnd);
                     }
 
                     currentSectionEnd += sectionLength;
                 }
 
-                foreach (Skill skill in skills) // todo figure out why 2 objects don't measure correctly using this whole thing
+                foreach (Skill skill in skills)
                     skill.Process(hitObject);
             }
-
-            // calculating the actual rating of each skill and the sum
+            
             double aimRating      = Math.Sqrt(skills[0].DifficultyValue()) * difficultyMultiplier;
             double speedRating    = Math.Sqrt(skills[1].DifficultyValue()) * difficultyMultiplier;
             double starRating     = aimRating + speedRating + Math.Abs(aimRating - speedRating) / 2;
--- starrating/standard/skills/Skill.cs
+++ starrating/standard/skills/Skill.cs
@@ -9,8 +9,6 @@ namespace MapsetParser.starrating.standard
 {
     abstract class Skill
     {
-        // works as a base class for all skills like aim, speed, etc.
-
         protected abstract double SkillMultiplier  { get; }    // how much this skill is weighed
         protected abstract double StrainDecay      { get; }    // how quickly strain decays for this skill
 
@@ -19,14 +17,15 @@ namespace MapsetParser.starrating.standard
         public double currentStrain        = 0;
         public double currentStrainPeak    = 0;
         
+        /// <summary> Returns how much to decay the strain over a given delta time in ms. </summary>
         public double GetStrainDecay(double aDeltaTime) => Math.Pow(StrainDecay, aDeltaTime / 1000);
 
         public List<HitObject>  previousObjects    = new List<HitObject>();
         public List<double>     strainPeaks        = new List<double>();
 
+        /// <summary> Covers base mechanics for strain, like decaying over time and increasing for each given object. </summary>
         public void Process(HitObject anObject)
         {
-            // base mechanics for strain, like decaying over time and increasing for each object provided
             currentStrain *= GetStrainDecay(anObject.GetPrevDeltaStartTime());
             if (!(anObject is Spinner))
                 currentStrain += StrainValueOf(anObject) * SkillMultiplier;
@@ -36,29 +35,28 @@ namespace MapsetParser.starrating.standard
             previousObjects.Add(anObject);
         }
 
+        /// <summary> Adds the current strain peak to a list, but only if at least one object was processed in total. </summary>
         public void SaveCurrentPeak()
         {
             if (previousObjects.Count > 0)
                 strainPeaks.Add(currentStrainPeak);
         }
 
+        /// <summary> Decays the current strain peak based on the given offset and time since last object. </summary>
         public void StartNewSectionFrom(double anOffset)
         {
-            // strain carries over to following sections
             if (previousObjects.Count > 0)
                 currentStrainPeak = currentStrain * GetStrainDecay(anOffset - previousObjects.Last().time);
         }
 
+        /// <summary> Returns the weighted total of peaks, where each is weighed 90% of the previous, starting from the highest. </summary>
         public double DifficultyValue()
         {
-            // sort highest > lowest
             strainPeaks.Sort((aStrain, anOtherStrain) => anOtherStrain.CompareTo(aStrain));
 
             double difficulty = 0;
             double weight     = 1;
             
-            // works similar to pp weighing as in every lower strain being weighed less
-            // in this case 90% of the previous
             foreach (double strainPeak in strainPeaks)
             {
                 difficulty += strainPeak * weight;
