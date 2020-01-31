fc2320f3c328f4ce6cc83d4e0fca2b4b3478f5ee
Naxesss <naxess.w@gmail.com>
2020-01-31 22:34:43 +0100

    Reformat Osb

--- objects/Osb.cs
+++ objects/Osb.cs
@@ -11,67 +11,65 @@ namespace MapsetParser.objects
     {
         public string code;
 
-        public readonly List<Background>     backgrounds;
-        public readonly List<Video>          videos;
-        public readonly List<Break>          breaks;
-        public readonly List<Sprite>         sprites;
-        public readonly List<StoryHitSound>  storyHitSounds;
-        public readonly List<Animation>      animations;
+        public readonly List<Background> backgrounds;
+        public readonly List<Video>      videos;
+        public readonly List<Break>      breaks;
+        public readonly List<Sprite>     sprites;
+        public readonly List<Sample>     samples;
+        public readonly List<Animation>  animations;
 
-        public Osb(string aCode)
+        public Osb(string code)
         {
-            code = aCode;
+            this.code = code;
 
-            string[] lines = aCode.Split(new string[] { "\n" }, StringSplitOptions.None);
+            string[] lines = code.Split(new string[] { "\n" }, StringSplitOptions.None);
 
             // substitute variables in the code before looking at the event part
             List<KeyValuePair<string, string>> substitutions = new List<KeyValuePair<string, string>>();
-            ParserStatic.ApplySettings(lines, "Variables", aSectionLines =>
+            ParserStatic.ApplySettings(lines, "Variables", sectionLines =>
             {
-                foreach (string line in aSectionLines)
+                foreach (string line in sectionLines)
                     if (line.StartsWith("$"))
                         substitutions.Add(new KeyValuePair<string, string>(
                             line.Split('=')[0].Trim(),
                             line.Split('=')[1].Trim()));
             });
 
-            string substitutedCode = aCode;
+            string substitutedCode = code;
             foreach (KeyValuePair<string, string> substitution in substitutions)
                 substitutedCode = substitutedCode.Replace(substitution.Key, substitution.Value);
 
             string codeResult = substitutedCode.ToString();
             string[] linesResult = codeResult.Split(new string[] { "\n" }, StringSplitOptions.None);
 
-            backgrounds    = GetEvents(linesResult, new List<string>() { "Background",   "0" }, anArgs => new Background(anArgs));
-            videos         = GetEvents(linesResult, new List<string>() { "Video",        "1" }, anArgs => new Video(anArgs));
-            breaks         = GetEvents(linesResult, new List<string>() { "Break",        "2" }, anArgs => new Break(anArgs));
-            sprites        = GetEvents(linesResult, new List<string>() { "Sprite",       "4" }, anArgs => new Sprite(anArgs));
-            storyHitSounds = GetEvents(linesResult, new List<string>() { "Sample",       "5" }, anArgs => new StoryHitSound(anArgs));
-            animations     = GetEvents(linesResult, new List<string>() { "Animation",    "6" }, anArgs => new Animation(anArgs));
+            backgrounds    = GetEvents(linesResult, new List<string>() { "Background",   "0" }, args => new Background(args));
+            videos         = GetEvents(linesResult, new List<string>() { "Video",        "1" }, args => new Video(args));
+            breaks         = GetEvents(linesResult, new List<string>() { "Break",        "2" }, args => new Break(args));
+            sprites        = GetEvents(linesResult, new List<string>() { "Sprite",       "4" }, args => new Sprite(args));
+            samples        = GetEvents(linesResult, new List<string>() { "Sample",       "5" }, args => new Sample(args));
+            animations     = GetEvents(linesResult, new List<string>() { "Animation",    "6" }, args => new Animation(args));
         }
 
         /// <summary> Returns whether the .osb file is actually used as a storyboard (or if it's just empty). </summary>
-        public bool IsUsed()
-        {
-            return backgrounds     .Count > 0
-                || videos          .Count > 0
-                || breaks          .Count > 0
-                || sprites         .Count > 0
-                || storyHitSounds  .Count > 0
-                || animations      .Count > 0;
-        }
+        public bool IsUsed() =>
+            backgrounds.Count > 0 ||
+            videos     .Count > 0 ||
+            breaks     .Count > 0 ||
+            sprites    .Count > 0 ||
+            samples    .Count > 0 ||
+            animations .Count > 0;
 
-        private List<T> GetEvents<T>(string[] aLines, List<string> aTypes, Func<string[], T> aFunc)
+        private List<T> GetEvents<T>(string[] lines, List<string> types, Func<string[], T> func)
         {
             // find all lines starting with any of aTypes in the event section
-            List<T> types = new List<T>();
-            ParserStatic.ApplySettings(aLines, "Events", aSectionLines =>
+            List<T> foundTypes = new List<T>();
+            ParserStatic.ApplySettings(lines, "Events", sectionLines =>
             {
-                foreach (string line in aSectionLines)
-                    if (aTypes.Any(aType => line.StartsWith(aType + ",")))
-                        types.Add(aFunc(line.Split(',')));
+                foreach (string line in sectionLines)
+                    if (types.Any(aType => line.StartsWith(aType + ",")))
+                        foundTypes.Add(func(line.Split(',')));
             });
-            return types;
+            return foundTypes;
         }
     }
 }
